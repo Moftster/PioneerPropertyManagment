9c774431a07c1d7d18902455cb58ee712bb28a37
David Moftakhar <david@sellmysnaps.com>
2019-09-16 20:39:30 +0100

    Setup project boilerplate and created jumbotron header.

---
+++ partials/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+
+<html lang="en">
+
+  <head>
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Pioneer Property Management</title>
+
+  </head>
+
+  <div class="jumbotron">
+    <h1>Pioneer Property Management</h1>
+
+    <nav class="navbar navbar-default navbar-fixed-top">
+           <ul class="nav navbar-nav navbar-right">
+               <li><a href="#">Home</a></li>
+               <li><a href="#services">Services</a></li>
+               <li><a href="#works">Works</a></li>
+               <li><a href="#about">About</a></li>
+               <li><a href="#contact">Contact</a></li>
+           </ul>
+    </nav>
+  </div>
