c7762086feb207932d3c8336c3cc6ad5b61c5d27
David Moftakhar <david@sellmysnaps.com>
2019-10-03 18:55:43 +0100

    Banner amended.

--- index.php
+++ index.php
@@ -10,82 +10,86 @@
           <div class="col-lg-4">
             <img class="icons-center" src="images/contract.png" width="140" height="140">
             <h2 class="text-center">Lettings</h2>
-            <p class="text-center"> Whether you're a landlord in need of advice or a tenant searching for a rental property we can help.</p>
+            <p class="text-center"> Advice and services for landlords and tenants searching for a rental property.</p>
           </div><!-- /.col-lg-4 -->
           <div class="col-lg-4">
             <img class="icons-center" src="images/house.png" width="140" height="140">
             <h2 class="text-center">Management</h2>
-            <p class="text-center">Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+            <p class="text-center">Professional property management services from rent managed to fully managed.</p>
           </div><!-- /.col-lg-4 -->
           <div class="col-lg-4">
             <img img class="icons-center" src="images/wrench.png" width="140" height="140">
             <h2 class="text-center">Maintenance</h2>
-            <p class="text-center">Donec sed odio dui. Dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          </div><!-- /.col-lg-4 -->
+            <p class="text-center">Residential and commercial building mantenance and high-quality refurbishment.</p>
         </div><!-- /.row -->
       </div>
+
+    </div>
 
 
     <!-- ABOUT -->
 
     <div id="about" class="home-section">
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-          <li data-target="#myCarousel" data-slide-to="1"></li>
-          <li data-target="#myCarousel" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-          <div class="carousel-item active">
-            <img class="slides first-slide" src="images/london-sky-2.jpg" alt="First slide">
-            <div class="container">
-              <div class="carousel-caption text-left">
-                <h1 class="banner-text">Example headline.</h1>
-                <p class="banner-text">Cras justo odio, dapibus ac facilisis in, egestas eget quam. </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="slides second-slide" src="images/interior.jpg" alt="Second slide">
-            <div class="container">
-              <div class="carousel-caption text-left">
-                <h1 class="banner-text">Another example headline.</h1>
-                <p class="banner-text">Cras justo odio, dapibus ac facilisis in, egestas eget quam. </p>
-              </div>
-            </div>
-          </div>
-          <div class="carousel-item">
-            <img class="slides third-slide" src="images/more-houses-3.jpg" alt="Third slide">
-            <div class="container">
-              <div class="carousel-caption text-left">
-                <h1 class="banner-text">One more for good measure.</h1>
-                <p class="banner-text">Cras justo odio, dapibus ac facilisis in, egestas eget quam. </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-          <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
-          <span class="carousel-control-next-icon" aria-hidden="true"></span>
-          <span class="sr-only">Next</span>
-        </a>
-      </div>
-
-      <hr>
-
       <div class="row featurette">
         <div class="col-md-5 order-md-2">
           <img class="featurette-image img-fluid mx-auto" src="images/mews.jpg" alt="Mews">
         </div>
          <div class="col-md-7 order-md-1">
-           <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-           <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+           <h2 class="featurette-heading">Pioneer<span class="text-muted">ing Property Management</span></h2>
+           <p class="lead">Pioneer provide a comprehensive suite of bespoke property services designed to make letting, managing and maintaining a property hassle-free and straightforward.</p>
+           <p class="lead">Focussing on residential and commerial property management in London and the South East of England, we remove the worry of day to day responsibilities giving landlords complete peace of mind and providing tenants a high-quality, safe and clean living environment that meets their expectations.
+          <p class="lead"> Over the past 20 years we have built an unrivalled reputation for quality and excellence in the industry giving landlords complete peace of mind and providing tenants with rental properties they are proud to call home.</p>
+          <p class="lead"> Whether you are looking to rent out your property or are searching for a new home don't hesitate to contact us.</p>
+
          </div>
 
        </div>
+       <hr>
+       <div id="myCarousel" class="carousel slide" data-ride="carousel">
+           <ol class="carousel-indicators">
+             <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+             <li data-target="#myCarousel" data-slide-to="1"></li>
+             <li data-target="#myCarousel" data-slide-to="2"></li>
+           </ol>
+           <div class="carousel-inner">
+             <div class="carousel-item active">
+               <img class="slides first-slide" src="images/banner3.jpg" alt="First slide">
+               <div class="container">
+
+                 <div class="carousel-caption text-left">
+                   <h1 class="banner-text">Better Service</h1>
+                   <p class="banner-text banner-text-p">Going the extra mile for tenants and landlords.</p>
+                 </div>
+               </div>
+             </div>
+             <div class="carousel-item">
+               <img class="slides second-slide" src="images/banner4.jpg" alt="Second slide">
+               <div class="container">
+                 <div class="carousel-caption text-left">
+                   <h1 class="banner-text">Hassle-Free</h1>
+                   <p class="banner-text banner-text-p">Working proactively to solve problems before they happen.</p>
+                 </div>
+               </div>
+             </div>
+             <div class="carousel-item">
+               <img class="slides third-slide" src="images/banner5.jpg" alt="Third slide">
+               <div class="container">
+                 <div class="carousel-caption text-left">
+                   <h1 class="banner-text">Industry leaders</h1>
+                   <p class="banner-text banner-text-p">Using knowlege and experience to deliver a quality service.</p>
+                 </div>
+               </div>
+             </div>
+           </div>
+           <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev">
+             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+             <span class="sr-only">Previous</span>
+           </a>
+           <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next">
+             <span class="carousel-control-next-icon" aria-hidden="true"></span>
+             <span class="sr-only">Next</span>
+           </a>
+         </div>
      </div>
 
 
--- partials/footer.php
+++ partials/footer.php
@@ -1,17 +1,14 @@
 </body>
 
-<footer id="footer">
-      <section>
-        <hr />
+</div>
+<div id="footer">
           <div class="text-center center-block">
             <a href="https://twitter.com/Moftster"><i class="fab fa-twitter-square fa-3x"></i></a>
             <a href="https://github.com/Moftster/politi-bot-v2"><i class="fab fa-github-square fa-3x"></i></a>
            </div>
-           <div class="footer-copyright text-center py-3">© 2019 Copyright: David Moftakhar <br>
-           Icons made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/"         title="Flaticon">www.flaticon.com</a></div>
-           <hr />
-      </section>
-  </footer>
+           <div class="footer-copyright text-center py-3">Web design by David Moftakhar © 2019 Copyright<br>
+</div>
+  </div>
 
 
 </html>
